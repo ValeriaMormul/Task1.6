049b391eaaada39aa7d9b5cb97f65d6e2c0040c6
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-02 01:24:50 +0200

    Добавьте файлы проекта.

---
+++ Task1.6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("23");
+        int number = int.Parse(Console.ReadLine());
+        if (number % 2 == 0)
+        {
+            Console.WriteLine($"Число {number} є парним");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number} не є парним");
+        }
+    }
+}
